c4ef9cc8aeee01202d9473620b54d07fc94dacb6
VyTK1 <vytk1@fsoft.com.vn>
2016-07-06 14:02:17 +0700

    Search Image

--- app/src/main/java/com/project/levi/news/PageDetailActivity.java
+++ app/src/main/java/com/project/levi/news/PageDetailActivity.java
@@ -1,13 +1,25 @@
 package com.project.levi.news;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PageDetailActivity extends AppCompatActivity {
+
+    /*@BindView(R.id.img_cover)
+    ImageView imgCover;*/
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_detail);
+        ButterKnife.bind(this);
+
     }
 }
